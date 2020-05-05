bd59096abc0d3c689a879fbf454c9ee1e282c817
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-05 23:32:35 +0530

    added author controller

---
+++ springwebapp/spring5webapp/src/main/java/com/example/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package com.example.spring5webapp.controllers;
+
+import com.example.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
