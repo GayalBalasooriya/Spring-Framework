a3199aafb3a24ec81a37b0d4227fb678fa4a453d
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-05 23:31:27 +0530

    initialize project

---
+++ sf-pet-clinic/src/test/java/com/example/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
