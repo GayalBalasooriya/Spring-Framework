226976e48e87eab28849fa9844d1f22a5d9d75f6
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-09 23:37:17 +0530

    DI added

---
+++ demo-di/src/main/java/com/example/demodi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.example.demodi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello() {
+        System.out.println("Hello");
+        return null;
+    }
+}
