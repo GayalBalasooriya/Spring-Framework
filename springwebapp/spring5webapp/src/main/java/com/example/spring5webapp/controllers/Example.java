45767ae191c2eb3932f65853566ef7e28feef8e3
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-10 23:25:19 +0530

    ..

--- springwebapp/spring5webapp/src/main/java/com/example/spring5webapp/controllers/Example.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring5webapp.controllers;
-
-import static jdk.nashorn.internal.objects.Global.print;
-
-public class Example {
-    void hi() {
-        print("asfg");
-    }
-}
